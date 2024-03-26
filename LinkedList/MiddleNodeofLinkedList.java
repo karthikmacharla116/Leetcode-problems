12b58a71b587573f7556af5bd1526af14228680a
Karthik <97028010+karthikmacharla116@users.noreply.github.com>
2024-03-26 10:31:54 +0530

    Create MiddleNodeofLinkedList.java

---
+++ LinkedList/MiddleNodeofLinkedList.java
@@ -0,0 +1,27 @@
+Problem Link: https://leetcode.com/problems/middle-of-the-linked-list/description/
+
+Hint: Apply Fast & Slow pointer
+
+Solution: 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class MiddleNodeofLinkedList {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
