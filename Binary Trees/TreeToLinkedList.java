4f5597cc1d8d85d8cada0dd7aa6aefb493d1ee6f
Karthik <97028010+karthikmacharla116@users.noreply.github.com>
2024-05-09 15:41:47 +0530

    Create TreeToLinkedList.java

---
+++ Binary Trees/TreeToLinkedList.java
@@ -0,0 +1,56 @@
+Problem Link:
+
+Intuition:
+  DFS, In-order traversal
+
+Solution:
+class ListNode {
+    int val;
+    ListNode prev;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+class TreeToLinkedList {
+
+    ListNode head;
+    ListNode tail;
+
+    public ListNode bstToDLL(TreeNode root) {
+    
+        inOrder(root);
+        return head;
+    }
+
+    public void inOrder(TreeNode node) {
+        if(node == null)
+            return;
+
+        inOrder(node.left);
+
+        ListNode temp = new ListNode(node.val);
+        if(head != null) {
+            tail.next = temp;
+            temp.prev = tail;
+            tail = temp;
+        } else {
+            head = temp;
+            tail = head;
+        }
+    
+    }
+
+}
