3b11dfddaf0019485247fe6de32010909543554f
Karthik <97028010+karthikmacharla116@users.noreply.github.com>
2024-08-27 09:10:56 +0530

    Create PathWithMaxProbability.java

---
+++ graph-questions/PathWithMaxProbability.java
@@ -0,0 +1,34 @@
+Problem Link: https://leetcode.com/problems/path-with-maximum-probability/description/?envType=daily-question&envId=2024-08-27
+
+Intuition:
+  Bellman ford Algorithm
+
+Solution:
+class PathWithMaxProbability {
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
+        double[] maxProb = new double[n];
+        maxProb[start] = 1.0;
+
+        for (int i = 0; i < n - 1; i++) {
+            boolean hasUpdate = false;
+            for (int j = 0; j < edges.length; j++) {
+                int u = edges[j][0];
+                int v = edges[j][1];
+                double pathProb = succProb[j];
+                if (maxProb[u] * pathProb > maxProb[v]) {
+                    maxProb[v] = maxProb[u] * pathProb;
+                    hasUpdate = true;
+                }
+                if (maxProb[v] * pathProb > maxProb[u]) {
+                    maxProb[u] = maxProb[v] * pathProb;
+                    hasUpdate = true;
+                }
+            }
+            if (!hasUpdate) {
+                break;
+            }
+        }
+
+        return maxProb[end];
+    }
+}
